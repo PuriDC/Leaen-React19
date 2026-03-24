2dc73300d9a4483b9439c2a06783e4d7d291fe24
PoohDev <puripat15game@gmail.com>
2026-03-25 00:40:50 +0700

    How to use pictures

--- src/components/PersonList.jsx
+++ src/components/PersonList.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
+import boy from "../assets/boy.svg";
+import girl from "../assets/girl.svg";
 function PersonList() {
   const [data, setData] = useState([
     { id: 1, name: "Pooh", gender: "Male" },
     { id: 2, name: "Poch", gender: "Male" },
     { id: 3, name: "Cream", gender: "Female" },
+    { id: 3, name: "Ploy", gender: "Female" }
   ]);
 
   const [show, setShow] = useState(true);
@@ -16,9 +19,8 @@ function PersonList() {
         {show &&
           data.map((item) => (
             <li key={item.id}>
-              <h3>
-                {item.id} | {item.name} | {item.gender}
-              </h3>
+                <img src={item.gender == "Male" ? boy : girl} width={50} height={50} />
+                <h3>{item.name}</h3>
             </li>
           ))}
       </ul>
