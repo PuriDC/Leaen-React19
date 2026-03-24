e4a3b2bdb3652c25e24d620c34d764bbda5b3298
PoohDev <puripat15game@gmail.com>
2026-03-24 23:49:29 +0700

    Array state

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import { useState } from "react";
 
 function App() {
-  const name = "Pooh";
-  const [age, setAge] = useState(20);
-
-  function add() {
-    setAge(age + 1);
-  }
-
+  const [data,setData]=useState([
+    {id:1,name:"Pooh",gender:"Male"},
+    {id:2,name:"Poch",gender:"Male"},
+    {id:3,name:"Cream",gender:"Female"}
+  ]);
   return (
     <>
-      <h1>Hello {name}</h1>
-      <h2>Age : {age} y</h2>
-      <button onClick={add}>+</button>{/* สามารถใช้เขียน function ไว้ด้านนอกแบบนี้ก็ได้หรือจะเขียนแบบบรรทัดถัดไปก็ได้แต่ถ้ามีคำสั่งหรือฟังชั่นมากกว่า 1 บรรทัดควรเขียนแยก*/}
-      <button onClick={()=>setAge(age-1)}>-</button>
-      <button onClick={()=>setAge(20)}>Clear</button>
+      <h1>จำนวนประชากร {data.length} คน</h1>      
+      <ul>
+        {data.map((item)=>(
+          <li key={item.id}> <h3>{item.id} | {item.name} | {item.gender}</h3></li>
+        ))}
+      </ul>
     </>
   );
 }
