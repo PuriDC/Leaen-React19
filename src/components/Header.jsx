cc08fe1395c395c4941d6f2d2fea220145cf5461
PoohDev <puripat15game@gmail.com>
2026-03-25 19:01:18 +0700

    props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import "./Header.css"
 
 // export default function Header(){
-const Header=()=>{
+const Header=(props)=>{
+    const {title} = props
     return(
         <nav>
-            <h1>My app</h1> 
+            <h1>{props.title}</h1> 
             <button>Light/Dark</button>
         </nav>
-    )
-}
+    );
+};
 // สามารเขียนแบบย่อส่วนแบบนี้ได้
 export default Header;
